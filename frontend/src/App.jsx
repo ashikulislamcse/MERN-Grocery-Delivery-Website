d3757e53b71022c408c57a90074e64175b23fb68
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-05 20:28:47 +0600

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import Home from "./Pages/Home";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import { useAppContext } from "./Context/AppContext";
+import Login from "./components/Login";
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("/seller");
+  const {showUserLogin} = useAppContext();
   return (
     <div>
       {isSellerPath ? "null" : <Navbar />}
+      {showUserLogin ? <Login/> : null}
       <Toaster />
       <div
         className={`${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
