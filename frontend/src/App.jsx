1ef36bc78e1ad9f41d88b9d2ec0a2d58c036b97d
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-05 20:10:47 +0600

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("/seller");
   return (
     <div>
       {isSellerPath ? "null" : <Navbar />}
-      <Toaster/>
+      <Toaster />
       <div
         className={`${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {isSellerPath ? "null" : <Footer />}
     </div>
   );
 };
