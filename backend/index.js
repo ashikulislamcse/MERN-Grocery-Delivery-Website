f55f17201363e3c963588d919ec4139dac048009
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-06 22:27:39 +0600

    Server Setup Complete

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import dotenv, { config } from 'dotenv'
+import express from "express";
+import dotenv, { config } from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOption));
 
-
-
-
-
-const PORT = process.env.PORT || 5001
-app.listen(PORT, (req, res)=>{
-    console.log(`Server in Running on Port: ${PORT}`);
-})
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, (req, res) => {
+  console.log(`Server in Running on Port: ${PORT}`);
+});
