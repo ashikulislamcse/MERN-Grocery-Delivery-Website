32750c904b8a581f11b89fa53b31a3fabeb2bb4b
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-07 20:28:33 +0600

    Updated

---
+++ backend/Controllers/productController.js
@@ -0,0 +1,23 @@
+
+
+export const addProduct = async(req, res)=>{
+    
+}
+
+
+
+export const productList = async(req, res)=>{
+
+}
+
+
+
+
+export const productById = async(req, res)=>{
+
+}
+
+
+export const changeStock = async(req, res)=>{
+
+}
