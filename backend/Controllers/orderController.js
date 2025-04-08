285a3fbfa1d486c1fec161556e724aefdfc9f8a9
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-08 11:24:08 +0600

    Updated

---
+++ backend/Controllers/orderController.js
@@ -0,0 +1,30 @@
+import Order from "../Models/orderModel.js";
+import Product from "../Models/productModel.js";
+
+export const placeOrderCOD = async (req, res) => {
+  try {
+    const { userId, items, address } = req.body;
+    if (!address || items.length === 0) {
+      res.json({ success: false, message: "Invalid Data" });
+    }
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+
+    amount += Math.floor(amount * 0.02);
+    await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "COD",
+    });
+    return res.json({ success: true, message: "Order Placed Successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
