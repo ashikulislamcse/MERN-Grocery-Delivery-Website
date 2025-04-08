de4a471db47b88a4020048fdc73d16d7db26532e
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-08 11:30:04 +0600

    Updated

--- backend/Controllers/orderController.js
+++ backend/Controllers/orderController.js
@@ -26,5 +26,18 @@ export const placeOrderCOD = async (req, res) => {
   }
 };
 
+export const getUserOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await Order.find({
+      userId,
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
 
-
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+  }
+};
