5953c664267ee724db423522e5099bdeb3a08f75
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-14 00:59:21 +0600

    ALL Orders Get for Seller

--- frontend/src/Pages/Seller/Orders.jsx
+++ frontend/src/Pages/Seller/Orders.jsx
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../../Context/AppContext";
-import { assets, dummyOrders } from "../../assets/assets";
+import { assets} from "../../assets/assets";
+import toast from "react-hot-toast";
 
 const Orders = () => {
-  const { currency } = useAppContext();
+  const { currency, axios, isSeller } = useAppContext();
   const [orders, setOrders] = useState([]);
 
   const fetchOrders = async () => {
-    setOrders(dummyOrders);
+    try {
+      const {data} = await axios.get("api/order/seller");
+      if (data.success) {
+        setOrders(data.orders);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
-    fetchOrders();
-  }, []);
+    if(isSeller){
+      fetchOrders();
+    }
+  }, [isSeller]);
   return (
     <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll">
       <div className="md:p-10 p-4 space-y-4">
