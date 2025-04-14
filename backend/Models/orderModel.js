693d56c8b7fdfcb9492445a5f0793d13121b2c12
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-14 13:51:22 +0600

    Updated

--- backend/Models/orderModel.js
+++ backend/Models/orderModel.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       required: true,
-      ref: "User",
     },
     items: [
       {
