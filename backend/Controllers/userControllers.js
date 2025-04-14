3fb5a39e3baed684b51ee2a20da2468a3d5d1886
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-14 16:07:35 +0600

    Updated

--- backend/Controllers/userControllers.js
+++ backend/Controllers/userControllers.js
@@ -81,7 +81,7 @@ export const Login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "None",
+        sameSite: "none",
         secure: true,
       })
       .json({
